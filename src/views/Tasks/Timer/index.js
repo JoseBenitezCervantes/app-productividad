12ae57d997771948e4687bbdc3818f8b864583d6
Jose Benitez <jose.benitez.cervantes@outlook.com>
2021-08-07 17:17:26 -0500

    Se agrega funcionalidad timer

--- src/views/Tasks/Timer/index.js
+++ src/views/Tasks/Timer/index.js
@@ -1,76 +1,126 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import SettingsBackupRestoreIcon from "@material-ui/icons/SettingsBackupRestore";
 import PauseIcon from "@material-ui/icons/Pause";
 import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
-import { Grid, IconButton, List, makeStyles } from '@material-ui/core';
+import { Grid, IconButton, List, makeStyles } from "@material-ui/core";
+import { useTimer } from "react-timer-hook";
 
 const useStyles = makeStyles((theme) => ({
-    leftAlignDialogActions: {
-      justifyContent: "flex-start",
-    },
-    appBar: {
-      position: "relative",
-      background: "#3f51b5",
-    },
-    title: {
-      marginLeft: theme.spacing(2),
-      flex: 1,
-      color: "white",
-    },
-    exit: {
-      color: "white",
-    },
-    formControl: {
-      margin: theme.spacing(1),
-      minWidth: 120,
-    },
-    fontTime: {
-      fontSize: "10rem",
-    },
-    timerIcons: {
-      textAlign: "center",
-    },
-    playIcon: {
-      textAlign: "right",
-    },
-    pauseIcon: {
-      textAlign: "center",
-    },
-    restartIcon: {
-      textAlign: "left",
-    },
-  }));
+  leftAlignDialogActions: {
+    justifyContent: "flex-start",
+  },
+  appBar: {
+    position: "relative",
+    background: "#3f51b5",
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+    color: "white",
+  },
+  exit: {
+    color: "white",
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  fontTime: {
+    fontSize: "10rem",
+  },
+  timerIcons: {
+    textAlign: "center",
+  },
+  playIcon: {
+    textAlign: "right",
+  },
+  pauseIcon: {
+    textAlign: "center",
+  },
+  restartIcon: {
+    textAlign: "left",
+  },
+}));
 
-  
-const Timer = ({detail}) => {
-console.log("🚀 ~ file: index.js ~ line 46 ~ Timer ~ detail", detail)
-    const classes = useStyles();
-    return (
-        <div>
-            <List>
-                <Grid container spacing={1} className={classes.timerIcons}>
-                  <Grid item xs={12}>
-                    <h1 className={classes.fontTime}>05:45</h1>
-                  </Grid>
-                  <Grid item xs={4} className={classes.playIcon}>
-                    <IconButton>
-                      <PlayCircleOutlineIcon fontSize={"large"} />
-                    </IconButton>
-                  </Grid>
-                  <Grid item xs={4} className={classes.pauseIcon}>
-                    <IconButton>
-                      <PauseIcon fontSize={"large"} />
-                    </IconButton>
-                  </Grid>
-                  <Grid item xs={4} className={classes.restartIcon}>
-                    <IconButton>
-                      <SettingsBackupRestoreIcon fontSize={"large"} />
-                    </IconButton>
-                  </Grid>
-                </Grid>
-              </List>
-        </div>
-    )
-}
+const Timer = ({ detail }) => {
+  const { task } = detail.data;
+  console.log("🚀 ~ file: index.js ~ line 47 ~ Timer ~ task", task);
+  const restHrs = task?.restTime[0];
+  const restMin = task?.restTime[1];
+  const classes = useStyles();
+  const timeI = new Date();
+  timeI.setHours(timeI.getHours() + restHrs);
+  timeI.setMinutes(timeI.getMinutes() + restMin);
+  const { seconds, minutes, hours, start, isRunning, pause, restart } =
+    useTimer({
+      expiryTimestamp: timeI,
+      autoStart: false,
+      onExpire: () => console.warn("onExpire called"),
+    });
 
-export default Timer
+  const updateStatus = (statusTask) => {
+    fetch(`${process.env.REACT_APP_SERVER_ARKON}/api/tasks/update`, {
+      method: "PUT",
+      body: JSON.stringify({
+        restTime: [hours, minutes],
+        id: task?._id,
+        statusTask,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+  };
+
+  useEffect(() => {
+    if ((seconds === 30 || seconds === 0) && isRunning) {
+      updateStatus("RUN");
+    }
+  }, [seconds]);
+  return (
+    <div>
+      <List>
+        <Grid container spacing={1} className={classes.timerIcons}>
+          <Grid item xs={12}>
+            <h1 className={classes.fontTime}>{`0${hours}:${
+              minutes.toString().length === 1 ? `0${minutes}` : minutes
+            }:${seconds}`}</h1>
+          </Grid>
+          <Grid item xs={4} className={classes.playIcon}>
+            <IconButton
+              onClick={() => {
+                updateStatus("RUN");
+                start();
+              }}
+            >
+              <PlayCircleOutlineIcon fontSize={"large"} />
+            </IconButton>
+          </Grid>
+          <Grid item xs={4} className={classes.pauseIcon}>
+            <IconButton
+              onClick={() => {
+                updateStatus("PAUSE");
+                pause();
+              }}
+            >
+              <PauseIcon fontSize={"large"} />
+            </IconButton>
+          </Grid>
+          <Grid item xs={4} className={classes.restartIcon}>
+            <IconButton
+              onClick={() => {
+                const timeRestart = new Date();
+                timeRestart.setHours(timeRestart.getHours() + restHrs);
+                timeRestart.setMinutes(timeRestart.getMinutes() + restMin);
+                updateStatus("RUN");
+                restart(timeRestart);
+              }}
+            >
+              <SettingsBackupRestoreIcon fontSize={"large"} />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </List>
+    </div>
+  );
+};
+
+export default Timer;
